6fae83483f977eb72fb1ac77e0817f4e4abf95bd
CreepyPip <semenlinckiy@gmail.com>
2026-03-05 18:27:23 +0900

    Add files via upload

--- test_time.cpp
+++ test_time.cpp
@@ -1,5 +1,16 @@
 #include "alg1.cpp"
 
+template <typename funcT> // Для принятия функции любого типа
+long long timertest(funcT func){
+
+	auto ns0 = steady_clock::now(); // Начало отсчёта наносекунд
+	func(); // Запуск проверочной функции
+	auto ns1 = steady_clock::now(); // Конец отсчёта наносекунд
+	long long ns = duration_cast<nanoseconds>(ns1 - ns0).count();
+
+	return ns;
+};
+
 int main()
 {
 
@@ -7,19 +18,27 @@ int main()
 	double min = -321;
 	double max = 321;
 
-	double mins = 0;
 	double maxs = 0.01;
+	long long ns = 0;
 
 	double *a = new double[n];
+
+	cout << "pipa" << endl;
 
 	for (int i = 0; i < 8; ++i)
 	{
 
 		a = RandMas(n, min, max);
 
-		timer([&]() {SearchMas(n, a, mins, maxs);});
+		ns = timertest([&]() {
+		for (int j = 0; j < 2000; ++j)
+		{
+			SearchMas(n, a, maxs);
+		}});
+
+		cout << ns << endl;
 		n = n + 50000;
 
-	}
+	};
 
 }
